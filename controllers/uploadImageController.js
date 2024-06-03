5dbd1fbd36e8ce4a0b0fd47cb552df7a8cf5981d
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-06-03 17:31:05 +0530

    updated controller functions for image upload to firebase

--- controllers/uploadImageController.js
+++ controllers/uploadImageController.js
@@ -1,84 +1,72 @@
 // Import dependencies
-import path from "node:path"; // Node.js module for path operations
-import crypto from "node:crypto"; // Node.js module for cryptographic functions
-import fs from "node:fs/promises"; // Node.js module for file system operations
 import multer from "multer"; // Middleware for handling file uploads
+import crypto from "node:crypto"; // Node.js module for cryptographic functions
 import sharp from "sharp"; // Node.js image processing library
+import path from "node:path"; // Node.js module for path operations
+import firebaseStorage from '../firebase.js'; // Import Firebase storage bucket
+import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage" // Firebase Storage functions
 
-// Define paths for storing draft and published images
-const DRAFT_IMAGE_PATH = "public/uploads/images/draft";
-const PUBLISHED_IMAGE_PATH = "public/uploads/images";
-
-// Configure storage for draft images
-const draftStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, DRAFT_IMAGE_PATH);
-  },
-  filename: function (req, file, cb) {
-    // Generate a unique filename based on MD5 hash, timestamp, and original filename
-    const hash = crypto
-      .createHash("md5")
-      .update(file.originalname)
-      .digest("hex")
-      .slice(0, 13);
-    const filename = `${hash}${Date.now()}${path.extname(file.originalname)}`;
-    cb(null, filename);
-  },
-});
-
-// Filter function to allow only specific file types
-const fileFilter = function (req, file, cb) {
-  let extension = path.extname(file.originalname).toLowerCase();
-
+// Multer file filter function to allow only specific file types
+const fileFilter = (req, file, cb) => {
   const allowedExtensions = [".jpeg", ".jpg", ".png", ".webp"];
 
-  if (allowedExtensions.includes(extension)) {
-    cb(null, true);
-  } else {
-    cb(
-      new Error(
-        "Invalid file type. Allowed file types are: .jpeg, .jpg, .png, .webp"
-      ),
-      false
-    );
+  // Check file extension
+  const extension = path.extname(file.originalname).toLowerCase();
+  if (!allowedExtensions.includes(extension)) {
+    return cb(new Error('Invalid file type. Allowed file types are: .jpeg, .jpg, .png, .webp'));
   }
+  // Pass file
+  cb(null, true);
 };
 
-// Create multer instances for draft and published uploads
-const draftUpload = multer({
-  storage: draftStorage,
+// Setting up multer as a middleware to handle uploads with memory storage
+const imageUpload = multer({
+  storage: multer.memoryStorage(),
   fileFilter: fileFilter,
 });
 
 // Controller function for handling draft image uploads
 const imageByFileDraft = async (req, res) => {
   try {
-    // Use multer to handle the single file upload
-    draftUpload.single("image")(req, res, async (error) => {
+    // Use multer to handle single file upload
+    imageUpload.single("image")(req, res, async (error) => {
       if (error) {
         return res.status(400).json({ error: error.message });
       }
 
-      // Generate a unique hash for the filename
+      // Generate a unique hash for filename
       const hash = crypto
         .createHash("md5")
         .update(req.file.originalname)
         .digest("hex")
         .slice(0, 13);
-      const compressedImagePath = `${DRAFT_IMAGE_PATH}/${hash}${Date.now()}.webp`;
 
-      // Process the uploaded image using sharp: convert to WebP format
-      await sharp(req.file.path).toFormat("webp").toFile(compressedImagePath);
+      // Create a unique filename with .webp extension
+      const imageName = `${hash}${Date.now()}.webp`;
 
-      // Remove the original image file
-      await fs.unlink(req.file.path);
+      // Process uploaded image using sharp to convert it to WebP format
+      const processedImageBuffer = await sharp(req.file.buffer).toFormat("webp").toBuffer();
 
-      // Send a success response with the compressed image URL
+      // Create a reference to Firebase storage for new image
+      const draftImageRef = ref(firebaseStorage, `files/uploads/images/draft/${imageName}`);
+
+      // Set metadata for uploaded image
+      const metadata = {
+        contentType: 'image/webp', // Ensure content type matches processed image format
+      };
+
+      // Upload processed image buffer to Firebase storage
+      const snapshot = await uploadBytes(draftImageRef, processedImageBuffer, metadata);
+
+      // Get download URL for uploaded image
+      const draftImageUrl = await getDownloadURL(snapshot.ref);
+
+      // Send a success response with URL of uploaded image
       await res.json({
-        message: "Image uploaded to draft successfully",
+        message: "Image saved to draft successfully",
         success: 1,
         file: {
-          url: `${process.env.SERVER_URI}/${compressedImagePath}`,
+          url: `${draftImageUrl}`,
         },
       });
     });
@@ -88,25 +76,46 @@ const imageByFileDraft = async (req, res) => {
   }
 };
 
+
 // Controller function for moving an image from draft to published
 const imageByFilePublished = async (req, res) => {
   try {
     const { imageURL } = req.body;
-    const imageName = imageURL.substring(imageURL.lastIndexOf("/") + 1);
 
-    // Define paths for draft and published images
-    const draftImagePath = `${DRAFT_IMAGE_PATH}/${imageName}`;
-    const publishedImagePath = `${PUBLISHED_IMAGE_PATH}/${imageName}`;
+    // Decode image URL to handle any encoded characters, then split to remove query parameters
+    const imagePath = decodeURIComponent(imageURL).split("?")[0];
 
-    // Move the image file from draft to published folder
-    await fs.rename(draftImagePath, publishedImagePath);
+    // Extract image name from URL path
+    const imageName = imagePath.substring(imagePath.lastIndexOf("/") + 1);
 
-    // Send a success response with the published image URL
-    res.json({
-      message: "Image moved to published folder successfully",
+    // Create references for draft and published images in Firebase Storage
+    const draftImageRef = ref(firebaseStorage, `files/uploads/images/draft/${imageName}`);
+    const publishedImageRef = ref(firebaseStorage, `files/uploads/images/${imageName}`);
+
+    // Get download URL of draft image
+    const draftImageUrl = await getDownloadURL(draftImageRef);
+
+    // Fetch image data from draft URL
+    const response = await fetch(draftImageUrl);
+    const imageData = await response.arrayBuffer();
+
+    // Upload image data to published folder in Firebase Storage
+    await uploadBytes(publishedImageRef, new Uint8Array(imageData), {
+      contentType: 'image/webp', // Ensure content type is correct
+    });
+
+    // Delete image from draft folder
+    await deleteObject(draftImageRef);
+
+    // Get download URL of published image
+    const publishedImageUrl = await getDownloadURL(publishedImageRef);
+
+    // Send a success response with published image URL
+    await res.json({
+      message: "Image published successfully",
       success: 1,
       file: {
-        url: `${process.env.SERVER_URI}/${publishedImagePath}`,
+        url: `${publishedImageUrl}`,
       },
     });
   } catch (error) {
